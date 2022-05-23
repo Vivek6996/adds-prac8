71ac0b475e36a69690071c37712101caed2f3051
Vivek6996 <73323788+Vivek6996@users.noreply.github.com>
2022-05-23 19:47:16 +0930

    a

--- main.cpp
+++ main.cpp
@@ -5,46 +5,52 @@
 #include "LinkedList.h"
 
 int main(){
-
   std::vector<std::string> li2 = { };
+
+  std::string in;
+  
+  getline(std::cin, in);
+  std::stringstream str_strm;
+
+  str_strm << in;
+
+  std::string temp_str;
+
+  while(!str_strm.eof()) 
+  {
+    str_strm >> temp_str;
+    li2.push_back(temp_str);
+  }
 
 
   int arr[100] = {};
 
-  int a = (li2.size()-3);
+  int a = (li2.size() - 3);
   for (int i = 0; i < a; i++) {
     arr[i] = stoi(li2[i]);
   }
 
-  std::string x = li2[li2.size() - 3];
+  std::string function = li2[li2.size() - 3];
   int ob = stoi(li2[li2.size() - 2]);
-  int ob2 = stoi(li2[li2.size()-1]);
+  int ob2 = stoi(li2[li2.size() - 1]);
 
   LinkedList * li = new LinkedList(arr, (li2.size() - 3));
   
-  if (x == "AF") 
-  {
+  if (function == "AF") {
     li ->addFront(ob);
-  } else if (x == "AE") 
-  {
+  } else if (function == "AE") {
     li ->addEnd(ob);
-  } else if (x == "AP") 
-  {
+  } else if (function == "AP") {
     li ->addAtPosition(ob, ob2);
-  } else if (x == "S") 
-  {
+  } else if (function == "S") {
     li ->search(ob);
-  } else if (x == "DF") 
-  {
+  } else if (function == "DF") {
     li ->deleteFront();
-  } else if (x == "DE") 
-  {
+  } else if (function == "DE") {
     li ->deleteEnd();
-  } else if (x == "DP") 
-  {
+  } else if (function == "DP") {
     li ->deletePosition(ob);
-  } else if (x == "GI") 
-  {
+  } else if (function == "GI") {
     li ->getItem(ob);
   } 
 
